54abc83e16b8946bc0dca1f85d76c8a7bcb400ac
Thomasjnliu <thomasjnliu@gmail.com>
2020-12-29 16:23:55 -0500

    Initial commit

---
+++ Assets/playerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D rb;
+
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y);
+    }
+}
