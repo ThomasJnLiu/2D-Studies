8e4c210f019025802ce33408d12bfc1c1847e1e0
Thomasjnliu <thomasjnliu@gmail.com>
2020-12-29 19:49:27 -0500

    Add movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    public float xOffset, yOffset, yCutoff , xCutoff;
+
+    public bool followPlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        if(player){
+            offset = transform.position - player.transform.position;
+            Debug.Log(offset);  
+        }
+
+    }
+    
+
+    void Update()
+    {
+        if(player && followPlayer){
+            transform.position = player.transform.position + offset + new Vector3(xOffset, yOffset, 0);
+
+        // If player position is outside of offsets, stop following
+        //     if(player.transform.position.y < yCutoff || player.transform.position.x < xCutoff){
+        //         followPlayer = false;
+        //     }
+        // }else if (player.transform.position.y > yCutoff && player.transform.position.x > xCutoff){
+        //                     followPlayer = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D rb;
+
+    public float moveSpeed, jumpForce;
+    private Vector2 moveDirection;
+    public Color debugColor = Color.red;
+    private CollisionState collisionState;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionState = GetComponent<CollisionState>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // Always call move function, pass move direction from OnMove
+      Move(moveDirection);
+    }
+
+    // Handles movement of player
+    void Move(Vector2 direction){
+      rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
+    }
+
+    // Called whenever move action is executed
+    public void OnMove(InputAction.CallbackContext context){
+      // Updates move direction with input direction
+      moveDirection = context.ReadValue<Vector2>();
+    }
+
+    // Called whenever jump action is executed
+    public void OnJump(){
+      // Check if touching ground before jumping
+      if(collisionState.standing){
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+      }
+    
+    }
+
+}
